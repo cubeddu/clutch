ad5c38a4ec46f5e6bd000e7b1dfa2720ab494f97
juan Cubeddu <juan@poeticsystems.com>
2016-05-25 17:25:49 -0500

    new updates

--- src/Clutch/Command/BaseCommand.php
+++ src/Clutch/Command/BaseCommand.php
@@ -2,185 +2,154 @@
 namespace Clutch\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Question\Question;
-use Symfony\Component\Console\Question\ChoiceQuestion;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-
 use DOMDocument;
 use DOMXPath;
+use Wa72\HtmlPageDom\HtmlPageCrawler;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Dumper;
 
 
-class BaseCommand
-{
 
-   function recurse_copy($src,$dst) {
-        $dir = opendir($src);
-        @mkdir($dst);
-        while(false !== ( $file = readdir($dir)) ) {
-            if (( $file != '.' ) && ( $file != '..' )) {
-                if ( is_dir($src . '/' . $file) ) {
-                  $this->recurse_copy($src . '/' . $file,$dst . '/' . $file);
-                }
-                else {
-                    copy($src . '/' . $file,$dst . '/' . $file);
-                }
-            }
+class ClutchCli{
+
+  
+  /**
+   * Move files from zip to theme
+   *
+   * @param $src, $dst
+   *   $src files from the zipfile
+   *   $dst where the files are goint to
+   *
+   * @return
+   *   render files to new location
+   */
+  function recurse_copy($src,$dst) {
+    $dir = opendir($src);
+    @mkdir($dst);
+    while(false !== ( $file = readdir($dir)) ) {
+      if (( $file != '.' ) && ( $file != '..' )) {
+        if ( is_dir($src . '/' . $file) ) {
+          $this->recurse_copy($src . '/' . $file,$dst . '/' . $file);
         }
-        closedir($dir);
+        else {
+          copy($src . '/' . $file,$dst . '/' . $file);
+        }
+      }
     }
+    closedir($dir);
+  }
 
-    function replace_tags($string, $vars){
-      return str_replace(array_keys($vars), $vars, $string);
-    }
+  /**
+   * Replace template variables
+   * with new theme content
+   *
+   * @param $string, $vars
+   *   replace keys with actual content
+   *
+   * @return
+   *   new template with theme name
+   */
+  function replace_tags($string, $vars){
+    return str_replace(array_keys($vars), $vars, $string);
+  }
 
-    function deleteDirectory($dirPath) {
-      if (is_dir($dirPath)) {
-        $objects = scandir($dirPath);
-        foreach ($objects as $object) {
-          if ($object != "." && $object !="..") {
-            if (filetype($dirPath . DIRECTORY_SEPARATOR . $object) == "dir") {
-              $this->deleteDirectory($dirPath . DIRECTORY_SEPARATOR . $object);
-            } else {
-              unlink($dirPath . DIRECTORY_SEPARATOR . $object);
-            }
+  function deleteDirectory($dirPath) {
+    if (is_dir($dirPath)) {
+      $objects = scandir($dirPath);
+      foreach ($objects as $object) {
+        if ($object != "." && $object !="..") {
+          if (filetype($dirPath . DIRECTORY_SEPARATOR . $object) == "dir") {
+            $this->deleteDirectory($dirPath . DIRECTORY_SEPARATOR . $object);
+          } else {
+            unlink($dirPath . DIRECTORY_SEPARATOR . $object);
           }
         }
-        reset($objects);
-        rmdir($dirPath);
       }
+      reset($objects);
+      rmdir($dirPath);
     }
-
-    function Directory($theme,$bundlezip){
-        $cssDir = "html/{$bundlezip}/css";
-        $jsDir = "html/{$bundlezip}/js";
-        $fontDir = "html/{$bundlezip}/fonts";
-        $imgDir = "html/{$bundlezip}/images";
-        $themecss = "{$theme}/css";
-        $themejs = "{$theme}/js";
-        $themefont = "{$theme}/fonts";
-        $themeimg = "{$theme}/images";
-        $tempInfo = __DIR__.'/../../../template';
-        // Create Theme folder.
-        if (!file_exists($theme)) {
-            mkdir($theme, 0777, true);
-        }
-        // Move files from zip to new theme.
-        if(!$cssDir){
-          $output->writeln('<comment>Failed to find CSS folder. make sure you are using the webflow zip</comment>');
-          return false;
-        }else{
-            $css = opendir($cssDir);
-          while(false !== ( $file = readdir($css)) ) {
-            if ( substr($file, -12) == '.webflow.css'){
-              rename( $cssDir.'/'.$file, $cssDir.'/'.$theme.substr($file, -12));
-            }
+  }
+  function copyWebflowFilesToTheme($tempDir, $themeDir, $theme, $folders_to_copy, $output) {
+    foreach($folders_to_copy as $folder) {
+      $folderPath = $tempDir.$folder;
+      if(!opendir($folderPath)) {
+        $output->writeln("<comment>Failed to find $folder folder. make sure you are using the webflow zip</comment>");
+        return FALSE;
+      }
+      if($folder == 'css') {
+        $css = opendir($folderPath);
+        while(false !== ( $file = readdir($css)) ) {
+          if ( substr($file, -12) == '.webflow.css'){
+            rename( $folderPath.'/'.$file, $folderPath.'/'.$theme.substr($file, -12));
           }
-        // Move files from zip rename with theme name.
-        $this->recurse_copy($cssDir,$themecss);
         }
-        if(!$jsDir){
-          $output->writeln('<comment>Failed to find JS folder. make sure you are using the webflow zip</comment>');
-          return false;
-        }else{
-          $this->recurse_copy($jsDir,$themejs);
-        }
-        if(!$fontDir){
-          $output->writeln('<comment>Failed to find Fonts folder. make sure you are using the webflow zip</comment>');
-          return false;
-        }else{
-          $this->recurse_copy($fontDir,$themejs);
-        }
-        if(!$imgDir){
-          $output->writeln('<comment>Failed to find Images folder. make sure you are using the webflow zip</comment>');
-          return false;
-        }else{
-          $this->recurse_copy($imgDir,$themejs);
-        }
-
-        $this->recurse_copy($tempInfo,$theme);
-        rename($theme.'/info.yml',$theme.'/'.$theme.'.info.yml');
-        rename($theme.'/libraries.yml',$theme.'/'.$theme.'.libraries.yml');
-        rename($theme.'/template.theme',$theme.'/'.$theme.'.theme');
+      }
+      // Move files from zip rename with theme name.
+      $this->recurse_copy($tempDir . $folder, $themeDir . $folder);
     }
-
-    function ThemeTemplates($theme, $vars){
-      $template = file_get_contents($theme.'/'.$theme.'.info.yml', true);
-      $infoYML = $this->replace_tags($template, $vars);
-      file_put_contents($theme.'/'.$theme.'.info.yml', $infoYML);
-
-      $template = file_get_contents($theme.'/'.$theme.'.libraries.yml', true);
-      $infoYML = $this->replace_tags($template, $vars);
-      file_put_contents($theme.'/'.$theme.'.libraries.yml', $infoYML);
-
-      $template = file_get_contents($theme.'/'.$theme.'.theme', true);
-      $infoYML = $this->replace_tags($template, $vars);
-      file_put_contents($theme.'/'.$theme.'.theme', $infoYML);
-    }
-
-    function Components($theme,$htmlfiles,$dataBundle,$bundle){
-          $files = array();
-    foreach($htmlfiles as &$file){
-      $bundle_file_name = basename($file,".html");
-      // echo $bundle_file_name."\r\n";
-      $html = file_get_contents($file);
-      $extracted_info = array(); //array to save all the info in just one array [data-component, data-field, div]
-      //Extract data-component and store it to $bundle_names
-      $data_bundles = explode($dataBundle.'="', $html);
-      $bundle_names = array();
-      foreach ($data_bundles as &$data_bundle) {
-        $data_bundle = substr($data_bundle, 0, strpos($data_bundle, '"'));
-        array_push($bundle_names, $data_bundle);
-      }
-      $doc = new DOMDocument;
-      @$doc->loadHTML($html);
-      $xpath = new DOMXPath($doc);
-      for ($i = 1; $i < count($bundle_names); $i++) { //Search for each data-component found
-        // echo $bundle_names[$i];
-        $result = '';
-        $query = '//div[@'.$dataBundle.'="' . $bundle_names[$i] . '"]/node()/..';
-        $node = $xpath->evaluate($query);
-        foreach ($node as $childNode) {
-          $result .= $doc->saveHtml($childNode); //store the div block to result line by line;
-        }
-        $temp = array($bundle_names[$i], $result);
-        array_push($extracted_info, $temp);
-      }
-      $extracted_node = array(); //array to save all the info in just one array [data-component, data-field, div]
-      //Extract data-component and store it to $bundle_names
-      //Generate files
-      $html_filename = basename($file);
-      $html_filename = str_replace('.html', '', $html_filename);
-      $theme_components = $theme."/".$bundle."/";
-      // var_dump($bundle_names);
-      if (!file_exists($theme_components)) {
-        mkdir($theme_components, 0777, true);
-      }
-      $page = $theme_components.$bundle.'.yml';
-      if(0 < count($bundle_names)){
-        $pageBundle = $bundle_file_name . ":\r\n  ";
-        $pageBundle .= $bundle.':' . "\r\n      ";
-        for ($j = 1; $j < count($bundle_names); $j++) {
-          $pageBundle .= $bundle_names[$j] . '' . "\r\n      ";
-        }
-        $pageBundle .= '' . "\r\n\r\n";
-      }
-      file_put_contents($page, $pageBundle, FILE_APPEND);
-
-      foreach ($extracted_info as &$info) {
-        if (!file_exists($theme_components . $info[0] )) {
-          mkdir($theme_components . $info[0] , 0777, true);
-        }
-        $filename = $theme_components . $info[0] . '/' . $info[0] . '.html.twig';
-        file_put_contents($filename, $info[1]);
-        // $output->writeln('<comment>'.$filename.' </comment>');
-         echo $filename. "\r\n";
+  }
+  function generateThemeTemplates($themeDir, $theme_vars) {
+    $templatesPath = getcwd() . '/' . drupal_get_path('module', 'clutch') .'/templates/';
+    $templates = array('info.yml', 'libraries.yml', 'template.theme');
+    foreach($templates as $file) {
+      $content = file_get_contents($templatesPath . $file);
+      $content = $this->replace_tags($content, $theme_vars);
+      if($file == 'template.theme') {
+        file_put_contents($themeDir.'/'.$theme_vars['{{themeMachine}}'].'.theme', $content);
+      }else {
+        file_put_contents($themeDir.'/'.$theme_vars['{{themeMachine}}'].".$file", $content);
       }
     }
+  }
+
+  function traverseFiles($temp_folder, $themeDir, $theme, $htmlfiles){
+    $pages = array();
+    foreach($htmlfiles as $file){
+      $file_name = basename($file,".html");
+      $page_machine_name = str_replace('-', '_', $file_name);
+      $pages[$page_machine_name] = array();
+      $this->generateComponents($temp_folder, $themeDir, $theme, $file, $pages, $page_machine_name);
+    }
+    $dumper = new Dumper();
+    $yaml = $dumper->dump($pages, 2);
+    file_put_contents($themeDir . '/blocks.yml', $yaml, FILE_APPEND);
+  }
+
+  function generateComponents($temp_folder, $themeDir, $theme, $file, &$pages, $page_machine_name) {
+    $drupal_types = array('block', 'node', 'menu', 'form');
+    foreach($drupal_types as $type) {
+      $this->generateComponent($temp_folder, $themeDir, $theme, $file, $pages, $page_machine_name, $type);
+    }
+  }
+
+  function generateComponent($temp_folder, $themeDir, $theme, $file, &$pages, $page_machine_name, $bundle) {
+    $temp_bundle_folder = $temp_folder . $bundle . 's/'; 
+    if(!file_exists($temp_bundle_folder)) {
+      mkdir($temp_bundle_folder);
     }
 
+    $html = file_get_contents($file);
+    $crawler = new HtmlPageCrawler($html);
+    $crawler->filterXPath("//*[@data-$bundle]")->each(function(HtmlPageCrawler $node, $i) use ($bundle, $temp_bundle_folder, &$pages, $page_machine_name) {
+      $data_bundle = $node->getAttribute("data-$bundle");
+      switch($bundle) {
+        case 'block':
+          $template = $temp_bundle_folder . 'block--block-content--' . str_replace('_', '-', $data_bundle) . '.html.twig';
+          $pages[$page_machine_name][] = $data_bundle;
+          break;
+        case 'node':
+          $template = $temp_bundle_folder . 'node--' . str_replace('_', '-', $data_bundle) . '.html.twig';
+          break;
+        case 'form':
+          $template = $temp_bundle_folder . 'form--contact-message--' . str_replace('_', '-', $data_bundle) . '.html.twig';
+          break;
+        case 'menu':
+          $template = $temp_bundle_folder . $data_bundle . '.html.twig';
+          break;
+      }
+      if(!file_exists($template)) {
+        file_put_contents($template, $node->saveHTML());
+      }
+    });
+  }
 }
--- src/Clutch/Command/CreateCommand.php
+++ src/Clutch/Command/CreateCommand.php
@@ -7,33 +7,33 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Question\Question;
-use Symfony\Component\Console\Question\ChoiceQuestion;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Clutch\Command\BaseCommand;
 use ZipArchive;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\DomCrawler\Crawler;
+use Wa72\HtmlPageDom\HtmlPageCrawler;
 
 class CreateCommand extends Command {
   protected function configure(){
     $this
-      ->setName('create:theme')
+      ->setName('generate')
       ->setDescription('This will generate your components folder')
       ->setDefinition(array(
-        new InputOption('zip-file', 'z', InputOption::VALUE_REQUIRED, 'Name of the zip file.'),
+        new InputOption('sitemap-url',
+        '-url', InputOption::VALUE_REQUIRED, 'Name of the zip file.'),
         new InputOption('theme-name', 't', InputOption::VALUE_REQUIRED, 'Theme Name'),
         new InputOption('theme-description', 'd', InputOption::VALUE_REQUIRED, 'Theme description'),
       ));
   }
 
-  protected function execute(InputInterface $input, OutputInterface $output){
-    // Webflow Zip Name
-    $bundlezip = $input->getOption('zip-file');
-    if(!$bundlezip){
+   protected function execute(InputInterface $input, OutputInterface $output) {
+    $zipFile = $input->getOption('sitemap-url');
+    if(!$zipFile){
       $helper = $this->getHelper('question');
-      $question = new Question('<info>Please enter the name of the zip file:</info> <comment>[webflow]</comment> ', 'webflow');
-      $bundlezip = $helper->ask($input, $output, $question);
+      $question = new Question('<info>Please enter your sitemap URL:</info> <comment>http://YOURSITE.webflow.io/sitemap.xml]</comment> ');
+      $zipFile = $helper->ask($input, $output, $question);
     }
-    $withZip = $bundlezip. ".zip";
+    $withZip = $zipFile. ".zip";
     // Theme Name
     $theme = $input->getOption('theme-name');
     if(!$theme){
@@ -41,36 +41,63 @@ class CreateCommand extends Command {
       $question = new Question('<info>Please enter theme name:</info> <comment>[webflow]</comment> ', 'webflow');
       $theme = $helper->ask($input, $output, $question);
     }
-    // Theme Description
-    $themeDesc = $input->getOption('theme-description');
-    if(!$themeDesc){
-      $helper = $this->getHelper('question');
-      $question = new Question('<info>Please enter theme description:</info> <comment>[These is a webflow theme]</comment> ', 'These is a webflow theme');
-      $themeDesc = $helper->ask($input, $output, $question);
-    }
-    // UnZip and extract to html folder.
-    $zip = new ZipArchive;
-    if ($zip->open($withZip) === TRUE) {
-      $zip->extractTo('html/');
-      $zip->close();
-      $output->writeln('<info>Starting Theme creation process</info>');
-    } else {
-      $output->writeln('<comment>Failed to open the archive!</comment>');
-      return false;
-    }
-    // Directory variables.
-    $directory = "html/{$bundlezip}/";
-    $htmlfiles = glob($directory . "*.html");
-    $themeMachine = strtolower(str_replace(" ","_",$theme));
+    
+    $themeDesc = 'These is a webflow theme';
+    
+    $tempPath = getcwd().'/temp';
 
-    $create = new BaseCommand;
-    $create->Directory($theme,$bundlezip);
-    $vars = array('{{themeName}}'=> $theme,'{{themeMachine}}'=> $themeMachine,'{{themeDescription}}'=> $themeDesc);
-    $create->ThemeTemplates($theme, $vars);
-    $create->Components($theme,$htmlfiles,'data-bundle','components');
-    $create->Components($theme,$htmlfiles,'data-node','nodes');
-    $create->deleteDirectory('html');
-    $output->writeln('<info>Good Job your theme is ready!</info>');
+    $sitemap = file_get_contents(trim($zipFile));
+    $crawler = new Crawler($sitemap);
+    $nodeValues = $crawler->filter('url')->each(function (Crawler $node, $i) {
+      return $node->text();
+    });
+
+    foreach($nodeValues as $site) {
+      $url = file_get_contents(trim($site));
+      $page = new HtmlPageCrawler($url);
+      $images = $page
+        ->filterXpath('//img')
+        ->extract(array('src'));
+          foreach ($images as $image) {
+              $path = basename($image);
+              $imageName = ltrim(stristr($path, '_'), '_');
+              $insert = file_put_contents('images', $imageName);
+              if (!$insert) {
+                  throw new \Exception('Failed to write image');
+              }
+          }
+        // echo $page->filter('div')->addClass('w-section')->setAttribute('data-block', 'that_block');
+    }
+    $style = $page->filterXpath('//style')->text(). "\r\n";
+    // $script = $page->filter('script')->attr('src'). "\r\n";
+    $script = $page->filter('script')->extract(array('src'));
+      foreach ($script as $src) {
+          echo $src;
+      }
+
+
+    $extract_webflow_dir = "$tempPath/$zipFile/";
+
+    $htmlfiles = array();
+
+    $finder = new Finder();
+    $finder->files()->name('*.html')->in($extract_webflow_dir);
+    foreach ($finder as $file) {
+      $htmlfiles[] = $file->getRealpath();
+    }
+    $theme_machine_name = strtolower(str_replace(" ","_",$theme));
+    $root = getcwd();
+    $themeDir = "$root/themes/$theme_machine_name/";
+    if(!file_exists($themeDir)) {
+      mkdir($themeDir, 0777);
+    }
+    $clutchCLI = new ClutchCli();
+    $folders_to_copy = array('blocks','nodes', 'menus', 'forms', 'css', 'js', 'fonts', 'images');
+    $clutchCLI->traverseFiles($extract_webflow_dir, $themeDir, $theme_machine_name, $htmlfiles);
+    $clutchCLI->copyWebflowFilesToTheme($extract_webflow_dir, $themeDir, $theme_machine_name, $folders_to_copy, $output);
+    $theme_vars = array('{{themeName}}'=> $theme,'{{themeMachine}}'=> $theme_machine_name,'{{themeDescription}}'=> $themeDesc);
+    $clutchCLI->generateThemeTemplates($themeDir, $theme_vars);
+    $clutchCLI->deleteDirectory($tempPath);
+    $output->writeln('<comment>'. "\r\n" .'Your theme '.$theme.' is now created.</comment>');      
   }
 }
-
